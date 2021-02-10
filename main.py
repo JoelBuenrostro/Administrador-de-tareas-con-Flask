e1b48faf8206fbbef3e2d5ac72b33275ef938d6a
JoelBuenrostro <joelbuenrostro@outlook.com>
2021-02-09 23:55:06 -0600

    Creacion de la clase Task para la base de datos

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,9 +6,23 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://database/task.db'
 db = SQLAlchemy(app)
 
 
+class Task(db.Model):
+    id = db.column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200))
+    done = db.Column(db.Boolean)
+
+
 @app.route('/')
 def index():
     return render_template("index.html")
+
+
+@app.route("/create-task", methods=['PST'])
+def create():
+    task = Task(content=request.form['content'], done=False)
+    db.session.add(task)
+    db.session.commit()
+    return 'Guardado'
 
 
 if __name__ == "__main__":
