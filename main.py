12b824ef85ae604ad4d6f82f175e309e1f4ac55e
JoelBuenrostro <joelbuenrostro@outlook.com>
2021-02-10 23:57:02 -0600

    Correccion error tipografico y clase Task

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://database/task.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 db = SQLAlchemy(app)
 
 
 class Task(db.Model):
-    id = db.column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200))
     done = db.Column(db.Boolean)
 
@@ -17,7 +18,7 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/create-task", methods=['PST'])
+@app.route("/create-task", methods=['POST'])
 def create():
     task = Task(content=request.form['content'], done=False)
     db.session.add(task)
