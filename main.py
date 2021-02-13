3af59c067cebb95d9d7708e4799c621ba6db41bb
JoelBuenrostro <joelbuenrostro@outlook.com>
2021-02-12 21:56:34 -0600

    Se agrega la ruta para borrar tareas y marcalas como completadas

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://database/task.db'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/tasks.db'
 db = SQLAlchemy(app)
 
 
@@ -14,16 +13,32 @@ class Task(db.Model):
 
 
 @app.route('/')
-def index():
-    return render_template("index.html")
+def home():
+    tasks = Task.query.all()
+    return render_template("index.html", tasks=tasks)
 
 
 @app.route("/create-task", methods=['POST'])
 def create():
-    task = Task(content=request.form['content'], done=False)
-    db.session.add(task)
+    new_task = Task(content=request.form['content'], done=False)
+    db.session.add(new_task)
     db.session.commit()
-    return 'Guardado'
+    return redirect(url_for('home'))
+
+
+@app.route("/done/<id>")
+def done(id):
+    task = Task.query.filter_by(id=int(id)).first()
+    task.done = not(task.done)
+    db.session.commit()
+    return redirect(url_for("home"))
+
+
+@app.route("/delete/<id>")
+def delete(id):
+    task = Task.query.filter_by(id=int(id)).delete()
+    db.session.commit()
+    return redirect(url_for("home"))
 
 
 if __name__ == "__main__":
